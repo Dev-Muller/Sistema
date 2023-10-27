778f36afbe879d9a953d1d9c7ed69d5cb380a5e7
muller <pedromullergl11@gmail.com>
2023-10-27 17:24:22 -0300

    feat: private methods implemented for GerenciamentoVotacao

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,5 +1,15 @@
 package com.betrybe.sistemadevotacao;
 
-public class GerenciamentoVotacao {
+import java.util.ArrayList;
 
+public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
+    private ArrayList<PessoaEleitora> pessoasEleitoras;
+    private ArrayList<PessoaCandidata> pessoasCandidatas;
+    private ArrayList<String> cpfsComputados;
+
+    public GerenciamentoVotacao(){
+        this.pessoasEleitoras = new ArrayList<PessoaEleitora>();
+        this.pessoasCandidatas = new ArrayList<PessoaCandidata>();
+        this.cpfsComputados = new ArrayList<String>();
+    }
 }
